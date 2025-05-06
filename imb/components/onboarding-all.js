f083040a27871d7bdba20313b542c13197f5c44a
Ryandi-S <93305443+Ryandi-S@users.noreply.github.com>
2025-05-06 18:54:15 +0700

    add stepper

--- imb/components/onboarding-all.js
+++ imb/components/onboarding-all.js
@@ -1,3 +1,31 @@
+function Stepper({ step, maxStep, className }) {
+  const renderItems = () => {
+      const elements = [];
+      for (let i = 0; i < maxStep; i++) {
+          elements.push(
+              <div
+                  key={i}
+                  className={
+                      `sm:w-12 w-full h-1 rounded-lg mr-1
+                      ${i < step
+                          ? "bg-fg-highlight-neutral"
+                          : "bg-bg-greyscale-subtle"}`
+                  }
+              />
+          );
+      }
+      return elements;
+  };
+
+  return (
+      <div className={`flex flex-row ${className}`}>{renderItems()}</div>
+  );
+}
+// Attach to window so Webflow can access it
+window.Stepper = Stepper;
+
+
+
 function PrimaryButton({
   onClick,
   arrow,
