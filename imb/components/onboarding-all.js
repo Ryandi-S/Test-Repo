2308e3da3548dbd246527e6ba74bd8b51bb629dc
Sahab habibi <sahab@aleph-labs.com>
2025-05-07 08:15:12 +0700

    personal detail page

--- imb/components/onboarding-all.js
+++ imb/components/onboarding-all.js
@@ -38,8 +38,8 @@ function PrimaryButton({ onClick, arrow, className, type, disabled, label }) {
       {arrow ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
+          width="20"
+          height="20"
           viewBox="0 0 24 24"
           fill="none"
         >
