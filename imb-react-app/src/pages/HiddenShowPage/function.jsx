b36f40a977f17a3d086da0f986c7f84a5ba6204f
Ryandi-S <93305443+Ryandi-S@users.noreply.github.com>
2025-05-14 14:00:48 +0700

    tes

--- imb-react-app/src/pages/HiddenShowPage/function.jsx
+++ imb-react-app/src/pages/HiddenShowPage/function.jsx
@@ -1,84 +1,83 @@
 const HiddenShowPageFunction = (ReactProp) => {
   const React = window.React || ReactProp;
   console.log("--- HiddenShowPageFunction ---");
-  console.log("show : ",showElement)
-  console.log("hide : ",hideElement)
 
+  // util functions
+  const showElement = (element) => {
+    if (element){
+      element.classList.add("imb-display-block");
+      element.classList.remove("imb-display-none");
+    }
+  }
+  const hideElement = (element) => {
+    if (element){
+      element.classList.remove("imb-display-block");
+      element.classList.add("imb-display-none");
+    }
+  }
+
+  // page functions
   const getToEmailPage = () => {
     const starterPage = document.querySelector("#onboarding-section-0");
     const emailPage = document.querySelector("#onboarding-section-1");
-    starterPage.classList.remove("imb-display-block");
-    starterPage.classList.add("imb-display-none");
-    emailPage.classList.add("imb-display-block");
+    hideElement(starterPage);
+    showElement(emailPage);
   }
-
   const getToPhonePage = () => {
     const emailPage = document.querySelector("#onboarding-section-1");
     const phonePage = document.querySelector("#onboarding-section-2");
-    emailPage.classList.remove("imb-display-block");
-    emailPage.classList.add("imb-display-none");
-    phonePage.classList.add("imb-display-block");
+    hideElement(emailPage);
+    showElement(phonePage);
   }
-
   const getToPersonalDetailsPage = () => {
     const phonePage = document.querySelector("#onboarding-section-2");
     const personalDetailsPage = document.querySelector("#onboarding-section-3");
-    phonePage.classList.remove("imb-display-block");
-    phonePage.classList.add("imb-display-none");
-    personalDetailsPage.classList.add("imb-display-block");
+    hideElement(phonePage);
+    showElement(personalDetailsPage);
   }
-
   const getToConfirmPage = () => {
     const personalDetailsPage = document.querySelector("#onboarding-section-3");
     const confirmPage = document.querySelector("#onboarding-section-4");
-    personalDetailsPage.classList.remove("imb-display-block");
-    personalDetailsPage.classList.add("imb-display-none");
-    confirmPage.classList.add("imb-display-block");
+    hideElement(personalDetailsPage);
+    showElement(confirmPage);
   }
-
   const getToSuccessPage = () => {
     const confirmPage = document.querySelector("#onboarding-section-4");
     const successPage = document.querySelector("#onboarding-section-5");
-    confirmPage.classList.remove("imb-display-block");
-    confirmPage.classList.add("imb-display-none");
-    successPage.classList.add("imb-display-block");
+    hideElement(confirmPage);
+    showElement(successPage);
   }
-
   const getToStarterPage = () => {
     const successPage = document.querySelector("#onboarding-section-5");
     const starterPage = document.querySelector("#onboarding-section-0");
-    successPage.classList.remove("imb-display-block");
-    successPage.classList.add("imb-display-none");
-    starterPage.classList.add("imb-display-block");
+    hideElement(successPage);
+    showElement(starterPage);
   }
 
+  // attach events
   React.useEffect(() => {
     const input = document.querySelector("#onboarding-0-button-signup");
+    const input2 = document.querySelector("#onboarding-1-button-next");
+    const input3 = document.querySelector("#onboarding-2-button-next");
+    const input4 = document.querySelector("#onboarding-3-button-next");
+    const input5 = document.querySelector("#onboarding-4-button-next");
+    const input6 = document.querySelector("#onboarding-success-button-begin");
+
     if (input) {
       input.addEventListener('click', getToEmailPage);
     }
-
-    const input2 = document.querySelector("#onboarding-1-button-next");
     if (input2) {
       input2.addEventListener('click', getToPhonePage);
     }
-
-    const input3 = document.querySelector("#onboarding-2-button-next");
     if (input3) {
       input3.addEventListener('click', getToPersonalDetailsPage);
     }
-
-    const input4 = document.querySelector("#onboarding-3-button-next");
     if (input4) {
       input4.addEventListener('click', getToConfirmPage);
     }
-
-    const input5 = document.querySelector("#onboarding-4-button-next");
     if (input5) {
       input5.addEventListener('click', getToSuccessPage);
     }
-
-    const input6 = document.querySelector("#onboarding-success-button-begin");
     if (input6) {
       input6.addEventListener('click', getToStarterPage);
     }
