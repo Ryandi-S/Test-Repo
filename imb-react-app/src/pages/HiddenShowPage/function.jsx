2052b91ace44c46fb620117592e1a231a53814b4
Ryandi-S <93305443+Ryandi-S@users.noreply.github.com>
2025-05-15 13:18:42 +0700

    tes

--- imb-react-app/src/pages/HiddenShowPage/function.jsx
+++ imb-react-app/src/pages/HiddenShowPage/function.jsx
@@ -2,18 +2,55 @@
 const HiddenShowPageFunction = (ReactProp) => {
   const React = window.React || ReactProp;
   console.log("--- HiddenShowPageFunction ---");
-  // page functions
 
+  // page elements
+  const starterPage = React.useRef(null);
+  const emailPage = React.useRef(null);
+  const phonePage = React.useRef(null);
+  const personalDetailsPage = React.useRef(null);
+  const confirmPage = React.useRef(null);
+  const successPage = React.useRef(null);
+  
+  const input = React.useRef(null);
+  const input2 = React.useRef(null);
+  const input3 = React.useRef(null);
+  const input4 = React.useRef(null);
+  const input5 = React.useRef(null);
+  const input6 = React.useRef(null);
+
+  React.useEffect(() => {
+    starterPage.current = document.querySelector("#onboarding-section-0");
+    emailPage.current = document.querySelector("#onboarding-section-1");
+    phonePage.current = document.querySelector("#onboarding-section-2");
+    personalDetailsPage.current = document.querySelector("#onboarding-section-3");
+    confirmPage.current = document.querySelector("#onboarding-section-4");
+    successPage.current = document.querySelector("#onboarding-section-5");
+
+    input.current = document.querySelector("#onboarding-0-button-signup");
+    input2.current = document.querySelector("#onboarding-1-button-next");
+    input3.current = document.querySelector("#onboarding-2-button-next");
+    input4.current = document.querySelector("#onboarding-3-button-next");
+    input5.current = document.querySelector("#onboarding-4-button-next");
+    input6.current = document.querySelector("#onboarding-success-button-begin");
+  }, []);
+
+  // page states
+  const [isValidated, setIsValidated] = React.useState(false);
+  const [formData, setFormData] = React.useState({
+    email: "",
+    phone: "",
+    firstName: "",
+    middleName: "",
+    lastName: ""
+  })
+
+  // page functions
   const getToEmailPage = () => {
-    const starterPage = document.querySelector("#onboarding-section-0");
-    const emailPage = document.querySelector("#onboarding-section-1");
-    hideElement(starterPage);
-    showElement(emailPage);
+    hideElement(starterPage.current);
+    showElement(emailPage.current);
   };
 
   const getToPhonePage = () => {
-    const emailPage = document.querySelector("#onboarding-section-1");
-    const phonePage = document.querySelector("#onboarding-section-2");
     const errorElement = document.querySelector(".imb-input-error-message");
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const inputEmailValue = document.querySelectorAll("input")[0].value;
@@ -22,21 +59,19 @@ const HiddenShowPageFunction = (ReactProp) => {
     } else {
       if (emailRegex.test(inputEmailValue)) {
         hideElement(errorElement);
-        hideElement(emailPage);
-        return showElement(phonePage);
+        hideElement(emailPage.current);
+        return showElement(phonePage.current);
       }
       return showElement(errorElement);
     }
   };
   const getToPersonalDetailsPage = () => {
-    const phonePage = document.querySelector("#onboarding-section-2");
-    const personalDetailsPage = document.querySelector("#onboarding-section-3");
-    const errorPhoneElement = phonePage.querySelector(
+    const errorPhoneElement = phonePage.current.querySelector(
       ".imb-input-error-message"
     );
     //number must be 10 digits and number only
     const phoneRegex = /^[0-9]{10}$/;
-    const inputPhoneBalue = phonePage.querySelectorAll("input")[0].value;
+    const inputPhoneBalue = phonePage.current.querySelectorAll("input")[0].value;
 
     if (inputPhoneBalue === "" || inputPhoneBalue === undefined) {
       return showElement(errorPhoneElement);
@@ -44,23 +79,21 @@ const HiddenShowPageFunction = (ReactProp) => {
       if (phoneRegex.test(inputPhoneBalue)) {
         console.log("valid phone number");
         hideElement(errorPhoneElement);
-        hideElement(phonePage);
-        return showElement(personalDetailsPage);
+        hideElement(phonePage.current);
+        return showElement(personalDetailsPage.current);
       }
       console.log("invalid phone number");
       return showElement(errorPhoneElement);
     }
   };
   const getToConfirmPage = () => {
-    const personalDetailsPage = document.querySelector("#onboarding-section-3");
-    const confirmPage = document.querySelector("#onboarding-section-4");
-    const firstName = personalDetailsPage.querySelector(
+    const firstName = personalDetailsPage.current.querySelector(
       "#onboarding-input-firstname"
     );
-    const middleName = personalDetailsPage.querySelector(
+    const middleName = personalDetailsPage.current.querySelector(
       "#onboarding-input-middlename"
     );
-    const lastName = personalDetailsPage.querySelector(
+    const lastName = personalDetailsPage.current.querySelector(
       "#onboarding-input-lastname"
     );
     const errorFirstNameElement = firstName.querySelector(
@@ -87,80 +120,36 @@ const HiddenShowPageFunction = (ReactProp) => {
       hideElement(errorFirstNameElement);
       hideElement(errorMiddleNameElement);
       hideElement(errorLastNameElement);
-      hideElement(personalDetailsPage);
-      return showElement(confirmPage);
+      hideElement(personalDetailsPage.current);
+      return showElement(confirmPage.current);
     }
   };
   const getToSuccessPage = () => {
-    const confirmPage = document.querySelector("#onboarding-section-4");
-    const successPage = document.querySelector("#onboarding-section-5");
-    hideElement(confirmPage);
-    showElement(successPage);
+    hideElement(confirmPage.current);
+    showElement(successPage.current);
   };
   const getToStarterPage = () => {
-    const successPage = document.querySelector("#onboarding-section-5");
-    const starterPage = document.querySelector("#onboarding-section-0");
-    hideElement(successPage);
-    showElement(starterPage);
+    hideElement(successPage.current);
+    showElement(starterPage.current);
   };
 
   // attach events
   React.useEffect(() => {
-    const input = document.querySelector("#onboarding-0-button-signup");
-    const input2 = document.querySelector("#onboarding-1-button-next");
-    const input3 = document.querySelector("#onboarding-2-button-next");
-    const input4 = document.querySelector("#onboarding-3-button-next");
-    const input5 = document.querySelector("#onboarding-4-button-next");
-    const input6 = document.querySelector("#onboarding-success-button-begin");
-
-    const errorElement = document.getElementsByClassName(
-      "imb-input-error-message"
-    );
-    if (errorElement) {
-      for (let i = 0; i < errorElement.length; i++) {
-        errorElement[i].classList.add("imb-display-none");
-      }
-    }
-
-    if (input) {
-      input.addEventListener("click", getToEmailPage);
-    }
-    if (input2) {
-      input2.addEventListener("click", getToPhonePage);
-    }
-    if (input3) {
-      input3.addEventListener("click", getToPersonalDetailsPage);
-    }
-    if (input4) {
-      input4.addEventListener("click", getToConfirmPage);
-    }
-    if (input5) {
-      input5.addEventListener("click", getToSuccessPage);
-    }
-    if (input6) {
-      input6.addEventListener("click", getToStarterPage);
-    }
+    input?.current.addEventListener("click", getToEmailPage);
+    input2?.current.addEventListener("click", getToPhonePage);
+    input3?.current.addEventListener("click", getToPersonalDetailsPage);
+    input4?.current.addEventListener("click", getToConfirmPage);
+    input5?.current.addEventListener("click", getToSuccessPage);
+    input6?.current.addEventListener("click", getToStarterPage);
 
     // Clean up to prevent memory leaks
     return () => {
-      if (input) {
-        input.removeEventListener("click", getToEmailPage);
-      }
-      if (input2) {
-        input2.removeEventListener("click", getToPhonePage);
-      }
-      if (input3) {
-        input3.removeEventListener("click", getToPersonalDetailsPage);
-      }
-      if (input4) {
-        input4.removeEventListener("click", getToConfirmPage);
-      }
-      if (input5) {
-        input5.removeEventListener("click", getToSuccessPage);
-      }
-      if (input6) {
-        input6.removeEventListener("click", getToStarterPage);
-      }
+      input?.current.removeEventListener("click", getToEmailPage);
+      input2?.current.removeEventListener("click", getToPhonePage);
+      input3?.current.removeEventListener("click", getToPersonalDetailsPage);
+      input4?.current.removeEventListener("click", getToConfirmPage);
+      input5?.current.removeEventListener("click", getToSuccessPage);
+      input6?.current.removeEventListener("click", getToStarterPage);
     };
   }, []);
 
--- imb-react-app/src/utils/utils.jsx
+++ imb-react-app/src/utils/utils.jsx
@@ -1,10 +1,12 @@
 const showElement = (element) => {
+  console.log("---> show : ",element);
   element.classList.add("imb-display-block");
   element.classList.remove("imb-display-none");
 }
 window.showElement = showElement;
 
 const hideElement = (element) => {
+  console.log("---> hide : ",element);
   element.classList.remove("imb-display-block");
   element.classList.add("imb-display-none");
 }
